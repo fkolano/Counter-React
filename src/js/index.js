c3a65b7ea8e58bbfcc265fe50779202323560ed5
fkolano <fkolano@gmail.com>
2022-06-20 14:43:19 +0000

    final commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,42 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 function SimpleCounter(props) {
-	return (
-		<div className="bigCounter">
-			<div className="clock">
-				<i className="far fa-clock"></i>
-			</div>
-			<div className="four">{props.digitFour % 10}</div>
-			<div className="three">{props.digitThree % 10}</div>
-			<div className="two">{props.digitTwo % 10}</div>
-			<div className="one">{props.digitOne % 10}</div>
-		</div>
-	);
+  return (
+    <div className="bigCounter">
+      <div className="clock">
+        <i className="far fa-clock"></i>
+      </div>
+      <div className="four">{props.digitFour % 10}</div>
+      <div className="three">{props.digitThree % 10}</div>
+      <div className="two">{props.digitTwo % 10}</div>
+      <div className="one">{props.digitOne % 10}</div>
+    </div>
+  );
 }
 SimpleCounter.propTypes = {
-	digitFour: PropTypes.number,
-	digitThree: PropTypes.number,
-	digitTwo: PropTypes.number,
-	digitOne: PropTypes.number,
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number,
 };
 let counter = 0;
 setInterval(function () {
-	const four = Math.floor(counter / 1000);
-	const three = Math.floor(counter / 100);
-	const two = Math.floor(counter / 10);
-	const one = Math.floor(counter / 1);
-	counter++;
-	ReactDOM.render(
-		<SimpleCounter
-			digitOne={one}
-			digitTwo={two}
-			digitThree={three}
-			digitFour={four}
-		/>,
-		document.querySelector("#app")
-	);
+  const four = Math.floor(counter / 1000);
+  const three = Math.floor(counter / 100);
+  const two = Math.floor(counter / 10);
+  const one = Math.floor(counter / 1);
+  counter++;
+  ReactDOM.render(
+    <SimpleCounter
+      digitOne={one}
+      digitTwo={two}
+      digitThree={three}
+      digitFour={four}
+    />,
+    document.querySelector("#app")
+  );
 }, 1000);
 //render your react application
-
-
-
-
-
-
-
-
-
-
-
-
-
